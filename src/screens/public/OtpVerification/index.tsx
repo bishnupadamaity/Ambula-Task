d9017a090da40312f586996e15b248977946b52e
Bishnupada Maity <bishnupdm.1@gmail.com>
2024-09-15 22:32:01 +0530

    arrange code

--- src/screens/public/OtpVerification/index.tsx
+++ src/screens/public/OtpVerification/index.tsx
@@ -1,13 +1,30 @@
-import { Box, HStack, Image, Input, Pressable, Row, Text, KeyboardAvoidingView, ScrollView, VStack, Toast } from 'native-base';
+import {
+    KeyboardAvoidingView,
+    ScrollView,
+    Pressable,
+    VStack,
+    HStack,
+    Image,
+    Input,
+    Toast,
+    Text,
+    Row,
+    Box,
+} from 'native-base';
 import React, { useRef, useState } from 'react';
-import { NativeSyntheticEvent, TextInput, TextInputKeyPressEventData, Platform, Keyboard } from 'react-native';
-import { useSelector } from 'react-redux';
-import AppIcon from '~/components/core/AppIcon';
-import { RootState } from '~/store';
+import {
+    TextInputKeyPressEventData,
+    NativeSyntheticEvent,
+    TextInput,
+    Platform,
+} from 'react-native';
 import { resetAndNavigate } from '~/utils/NavigationUtils';
+import AppIcon from '~/components/core/AppIcon';
+import { useSelector } from 'react-redux';
+import { RootState } from '~/store';
 
 const OtpVerification = () => {
-    const user = useSelector((state: RootState) => (state.user));
+    const user = useSelector((state: RootState) => state.user);
     const lastThreeDigits = user.phone.slice(-3); // Get the last three digits
     const maskedNumber = '*******' + lastThreeDigits;
     const [otp, setOtp] = useState(['', '', '', '', '', '']);
@@ -27,19 +44,19 @@ const OtpVerification = () => {
         }
     };
 
-    const handleKeyPress = (e: NativeSyntheticEvent<TextInputKeyPressEventData>, index: number) => {
+    const handleKeyPress = (
+        e: NativeSyntheticEvent<TextInputKeyPressEventData>,
+        index: number,
+    ) => {
         if (e.nativeEvent.key === 'Backspace' && otp[index] === '' && index > 0) {
             inputRefs.current[index - 1]?.focus();
         }
     };
 
     const handleVerify = () => {
-
-        if (otp.every((digit) => digit.length === 1)) {
-
+        if (otp.every(digit => digit.length === 1)) {
             resetAndNavigate('TabLayout');
         } else {
-
             Toast.show({ title: 'Please enter all 6 digits', status: 'error' } as any);
         }
     };
@@ -48,12 +65,10 @@ const OtpVerification = () => {
         <KeyboardAvoidingView
             flex={1}
             behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}
-        >
+            keyboardVerticalOffset={Platform.OS === 'ios' ? 80 : 0}>
             <ScrollView
                 // contentContainerStyle={{ flexGrow: 1 }}
-                keyboardShouldPersistTaps="handled"
-            >
+                keyboardShouldPersistTaps="handled">
                 <Box flex={1} justifyContent="center" bg={'violet.50'}>
                     <Row alignItems={'center'} p={4} bg={'violet.100'}>
                         <Pressable
@@ -68,23 +83,49 @@ const OtpVerification = () => {
                             </Text>
                         </Box>
                     </Row>
-                    <Box alignItems={'center'} bg={'violet.100'} py={4} borderBottomRadius={'3xl'}>
-                        <Text fontWeight={'medium'} color={'coolGray.600'}>Enter the 6-digit OTP sent to your mobile number</Text>
-                        <Text fontWeight={'medium'} color={'blue.700'}>{maskedNumber}</Text>
+                    <Box
+                        alignItems={'center'}
+                        bg={'violet.100'}
+                        py={4}
+                        borderBottomRadius={'3xl'}>
+                        <Text fontWeight={'medium'} color={'coolGray.600'}>
+                            Enter the 6-digit OTP sent to your mobile number
+                        </Text>
+                        <Text fontWeight={'medium'} color={'blue.700'}>
+                            {maskedNumber}
+                        </Text>
                     </Box>
                     <Box alignItems={'center'} mt={2}>
-                        <Image source={{ uri: 'https://cdn-icons-png.flaticon.com/256/8046/8046321.png' }} style={{ width: 240, height: 240 }} resizeMode='contain' alt='security' />
+                        <Image
+                            source={{
+                                uri: 'https://cdn-icons-png.flaticon.com/256/8046/8046321.png',
+                            }}
+                            style={{ width: 240, height: 240 }}
+                            resizeMode="contain"
+                            alt="security"
+                        />
                     </Box>
-                    <Box w={'90%'} alignItems={'center'} alignSelf={'center'} justifyContent={'center'} rounded={'xl'} p={4} bg={'white'} borderWidth={0} borderColor={'blue.200'} >
-                        <Text fontWeight={'medium'} color={'gray.400'} my={3}>Enter OTP</Text>
+                    <Box
+                        w={'90%'}
+                        alignItems={'center'}
+                        alignSelf={'center'}
+                        justifyContent={'center'}
+                        rounded={'xl'}
+                        p={4}
+                        bg={'white'}
+                        borderWidth={0}
+                        borderColor={'blue.200'}>
+                        <Text fontWeight={'medium'} color={'gray.400'} my={3}>
+                            Enter OTP
+                        </Text>
                         <HStack space={2} justifyContent="center">
                             {otp.map((_, index) => (
                                 <Input
                                     key={index}
                                     value={otp[index]}
-                                    onChangeText={(text) => handleChangeText(text, index)}
-                                    onKeyPress={(e) => handleKeyPress(e, index)}
-                                    ref={(ref) => (inputRefs.current[index] = ref)}
+                                    onChangeText={text => handleChangeText(text, index)}
+                                    onKeyPress={e => handleKeyPress(e, index)}
+                                    ref={ref => (inputRefs.current[index] = ref)}
                                     maxLength={1}
                                     keyboardType="number-pad"
                                     textAlign="center"
@@ -93,29 +134,68 @@ const OtpVerification = () => {
                                     variant="outline"
                                     bg="white"
                                     _focus={{
-                                        borderColor: 'black',        // Set the border color to black when active
+                                        borderColor: 'black', // Set the border color to black when active
                                         backgroundColor: 'transparent', // Disable active background color
                                     }}
                                 />
                             ))}
                         </HStack>
-                        <Pressable onPress={handleVerify} my={3} bg={'blue.600'} p={3} rounded={'xl'} w={'full'} mt={6} alignItems={'center'}>
-                            <Text fontWeight={'medium'} color={'white'} >Verify & Continue</Text>
+                        <Pressable
+                            onPress={handleVerify}
+                            my={3}
+                            bg={'blue.600'}
+                            p={3}
+                            rounded={'xl'}
+                            w={'full'}
+                            mt={6}
+                            alignItems={'center'}>
+                            <Text fontWeight={'medium'} color={'white'}>
+                                Verify & Continue
+                            </Text>
                         </Pressable>
                     </Box>
-                    <Row justifyContent={'space-between'} alignItems={'center'} w={'90%'} alignSelf={'center'} px={4} mt={1}>
-                        <Text fontWeight={'medium'} color={'gray.500'} >Didn't receive the OTP?</Text>
+                    <Row
+                        justifyContent={'space-between'}
+                        alignItems={'center'}
+                        w={'90%'}
+                        alignSelf={'center'}
+                        px={4}
+                        mt={1}>
+                        <Text fontWeight={'medium'} color={'gray.500'}>
+                            Didn't receive the OTP?
+                        </Text>
                         <Pressable _pressed={{ opacity: 0.5 }}>
-                            <Text fontWeight={'medium'} color={'blue.600'}>Resend OTP</Text>
+                            <Text fontWeight={'medium'} color={'blue.600'}>
+                                Resend OTP
+                            </Text>
                         </Pressable>
                     </Row>
-                    <VStack p={4} alignItems={'center'} justifyContent={'center'} alignSelf={'center'}>
-                        <Text color={'gray.500'} fontSize={'xs'} my={2} >Approved & Authorized By</Text>
+                    <VStack
+                        p={4}
+                        alignItems={'center'}
+                        justifyContent={'center'}
+                        alignSelf={'center'}>
+                        <Text color={'gray.500'} fontSize={'xs'} my={2}>
+                            Approved & Authorized By
+                        </Text>
                         <Row alignItems={'center'} space={4}>
-                            <Image source={{ uri: 'https://abdm.gov.in:8081/uploads/ndhm_logo_65d00d9518.png' }} style={{ width: 60, height: 40 }} resizeMode='contain' alt='National Health Authority' />
-                            <Image source={{ uri: 'https://abdm.gov.in:8081/uploads/logo_1c71441e1d.png' }} style={{ width: 30, height: 40 }} resizeMode='contain' alt='national health authority' />
+                            <Image
+                                source={{
+                                    uri: 'https://abdm.gov.in:8081/uploads/ndhm_logo_65d00d9518.png',
+                                }}
+                                style={{ width: 60, height: 40 }}
+                                resizeMode="contain"
+                                alt="National Health Authority"
+                            />
+                            <Image
+                                source={{
+                                    uri: 'https://abdm.gov.in:8081/uploads/logo_1c71441e1d.png',
+                                }}
+                                style={{ width: 30, height: 40 }}
+                                resizeMode="contain"
+                                alt="national health authority"
+                            />
                         </Row>
-
                     </VStack>
                 </Box>
             </ScrollView>
